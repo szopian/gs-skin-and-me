38799d0d580dfed67c7a77cced5e7407f106b4e7
Adri Ana <szopiangreg@gmail.com>
2021-07-30 13:22:10 +0100

    add images folder working on card one

--- skin-and-me-react-app/src/components/CardOne.js
+++ skin-and-me-react-app/src/components/CardOne.js
@@ -2,14 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import ButtonOne from "./ButtonOne";
 
-// font-family: 'Raleway', sans-serif;
-// font-family: 'Roboto', sans-serif;
-
 const Card = styled.div`
   max-width: 879px;
   height: 300.2px;
   padding: 100px 290px;
 `;
+
+const Container = styled.div``;
 
 const Title = styled.h1`
   width: 384px;
@@ -34,22 +33,26 @@ const Content = styled.p`
   font-size: 16px;
   line-height: 21px;
   margin-bottom: 22px;
-
   /* Off Black */
   color: #0b0c0c;
 `;
 
+const Img = styled.img``;
+
 function CardOne() {
   return (
     <Card>
-      <Title>Meet the products chosen for you</Title>
-      <Content>
-        To get the best out of your Skin + Me treatment, it’s all about keeping
-        things simple. Discover our recommended routine – with what to use and
-        when to use it, with the products our expert Dermatology Team recommend
-        for you and your skin.
-      </Content>
-      <ButtonOne />
+      <Container>
+        <Title>Meet the products chosen for you</Title>
+        <Content>
+          To get the best out of your Skin + Me treatment, it’s all about
+          keeping things simple. Discover our recommended routine – with what to
+          use and when to use it, with the products our expert Dermatology Team
+          recommend for you and your skin.
+        </Content>
+        <ButtonOne />
+      </Container>
+      <Img />
     </Card>
   );
 }
